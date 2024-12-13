4bd1cc0be111f94d6873d6503452b1195f7871b3
TatianaAll <tatiana.allery@orange.fr>
2024-12-13 15:05:59 +0100

    crud for categories

--- src/Controller/admin/AdminCategoryController.php
+++ src/Controller/admin/AdminCategoryController.php
@@ -4,15 +4,17 @@ namespace App\Controller\admin;
 
 use App\Entity\Category;
 use App\Form\AdminCreateCategoryType;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminCategoryController extends AbstractController
 {
     #[Route(path: '/admin/create/category', name: 'admin_create_category', methods: ['POST', 'GET'])]
-    public function adminCreateCategory(Request $request, EntityManagerInterface $entityManager)
+    public function adminCreateCategory(Request $request, EntityManagerInterface $entityManager) : Response
     {
         //dd('test route');
         $category = new Category();
@@ -30,6 +32,61 @@ class AdminCategoryController extends AbstractController
 
             return $this->redirectToRoute('admin_create_category');
         }
-        return $this->render('admin/createCategory.html.twig', ['formView' => $formView]);
+        return $this->render('admin/categories/createCategory.html.twig', ['formView' => $formView]);
+    }
+
+    #[Route(path: '/admin/categories/list', name: 'admin_list_categories', methods: ['GET'])]
+    public function adminListRecipes(CategoryRepository $categoryRepository): Response
+    {
+        //dd('test');
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('admin/categories/listCategories.html.twig', ['categories' => $categories]);
+    }
+
+    #[Route(path: '/admin/update/category/{id}', name: 'admin_update_category',requirements: ['id'=>'\d+'], methods: ['POST', 'GET'])]
+    public function adminUpdateCategory(int $id, Request $request, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository)
+    {
+        //dd('test route');
+        $categoryToUpdate = $categoryRepository->find($id);
+
+        if(!$categoryToUpdate) {
+            $this->addFlash('error', 'Catégorie introuvable');
+            $this -> redirectToRoute('admin_show_category');
+        }
+
+        $formAdminCategory = $this->createForm(AdminCreateCategoryType::class, $categoryToUpdate);
+        $formAdminCategory->handleRequest($request);
+
+        $formView = $formAdminCategory->createView();
+
+        if($formAdminCategory->isSubmitted()) {
+            $entityManager->persist($categoryToUpdate);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Catégorie modifiée');
+
+            return $this->redirectToRoute('admin_list_categories');
+        }
+        return $this->render('admin/categories/updateCategory.html.twig', ['formView' => $formView, 'categoryToUpdate'=>$categoryToUpdate]);
+    }
+
+    #[Route(path:'/admin/delete/category/{id}', name: 'admin_delete_category', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    public function adminDeleteRecipe(int $id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        //dd('test route');
+        $categoryToDelete = $categoryRepository->find($id);
+        if(!$categoryToDelete) {
+            $this->addFlash('error', 'Cette catégorie n\'existe pas');
+            $this->redirectToRoute('admin_list_categories');
+        }
+
+        //dd($recipe);
+        $entityManager->remove($categoryToDelete);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Catégorie supprimée avec succès !');
+
+        return $this->redirectToRoute('admin_list_categories');
     }
 }
